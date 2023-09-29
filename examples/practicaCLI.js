739b335dc122dccc7391427e28b43f9ba3842b9f
tjess25 <88819417+tjess25@users.noreply.github.com>
2023-09-28 22:22:42 -0600

    Create practicaCLI.js

---
+++ examples/practicaCLI.js
@@ -0,0 +1,99 @@
+//Practica realizada por Jherson Gallo
+
+const fs = require('fs');
+
+const nombreCommand = process.argv[2];
+const nombreFile = process.argv[3];
+
+// `./${nombreFile}` == "./"+nombreFile
+
+const createDirectotyInit = (nombreFile)=>{
+    fs.mkdir(`./${nombreFile}`, (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Directory created successfully!');
+        fs.writeFile(`./${nombreFile}/index.html`, '', (err) => {
+            if (err) throw err;
+            console.log('Archivo creado Satisfactoriamente');
+        });
+    });
+}
+const createDirectoty = (nombreFile)=>{
+    fs.mkdir(`./${nombreFile}`, (err) => {
+        if (err) {
+            return console.error(err);
+        }
+    });
+}
+
+const createHtml = ()=>{
+    fs.writeFile(`./${nombreFile}.html`, '', (err) => {
+        if (err) throw err;
+        console.log('Archivo creado Satisfactoriamente');
+    });
+}
+
+const doesItExist = (type) =>{
+    return fs.readdir(`.`,
+    { withFileTypes: false },
+    (err, files) => {
+        console.log("\nCurrent directory files:");
+        if (err) //{throw err}
+            console.log(err);
+        else {
+            files.forEach(file => {
+                if(file === type){
+                    return true
+                }
+            })
+            return false;
+        }
+    })
+}
+
+const createJs = ()=>{
+    if(!doesItExist('js')){
+        createDirectoty('js')
+        fs.writeFile(`./js/${nombreFile}.js`, '', (err) => {
+            if (err) throw err;
+            console.log('Archivo creado Satisfactoriamente');
+        });
+    }else{
+        fs.writeFile(`./${nombreFile}.js`, '', (err) => {
+            if (err) throw err;
+            console.log('Archivo creado Satisfactoriamente');
+        });
+    }
+}
+
+const createCss = ()=>{
+    if(!doesItExist('css')){
+        createDirectoty('css')
+        fs.writeFile(`./css/${nombreFile}.css`, '', (err) => {
+            if (err) throw err;
+            console.log('Archivo creado Satisfactoriamente');
+        });
+    }else{
+        fs.writeFile(`./${nombreFile}.css`, '', (err) => {
+            if (err) throw err;
+            console.log('Archivo creado Satisfactoriamente');
+        });
+    }
+}
+
+
+
+switch(nombreCommand){
+    case 'init':
+        createDirectotyInit(nombreFile)
+    break;
+    case 'html': 
+    createHtml(nombreFile)
+    break;
+    case 'js':
+    createJs(nombreFile)
+    break;
+    case 'css':
+    createCss(nombreFile)
+}
